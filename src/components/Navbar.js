414c0eecb87a7473f590730eb79456fc9fd6251f
SHRAVANI KAWARE <Shravanikaware12@gmail.com>
2024-03-13 22:32:00 +0530

    navbar done

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../main.css";
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<h3>LOGO</h3>
+			<nav ref={navRef}>
+				<a href="/#">Portfolio</a>
+				<a href="/#">Market Place</a>
+				<a href="/#">Assets</a>
+				<a href="/#">About US</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
+
+export default Navbar;
