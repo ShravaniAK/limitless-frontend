0f14ba761cca98da359de3bea8ac7d3dc4d04aa3
SHRAVANI KAWARE <Shravanikaware12@gmail.com>
2024-03-17 20:40:10 +0530

    commit b

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -31,29 +31,6 @@ const Assets = () => {
   });
 
   return (
-<<<<<<< HEAD
-    <div className="container flex flex-col  mx-auto p-4">
-      <div className="flex flex-col items-center mb-4">
-        <input
-          type="text"
-          placeholder="Search assets..."
-          value={searchTerm}
-          onChange={handleSearchChange}
-          className="w-64 p-2 border border-gray-300 rounded mr-4"
-        />
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {filteredAssets.map(asset => (
-          <Link to={`/assets/${asset._id}`} key={asset._id} className="block">
-            <div className="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-              <img src={asset.assetId.logo} alt={asset.assetId.name} className="w-full h-48 object-cover rounded-t-lg" />
-              <div className="p-4">
-                <h2 className="text-lg font-semibold">{asset.assetId.name}</h2>
-                <p className="text-gray-600">{asset.description}</p>
-                <p>Ticker: {asset.assetId.ticker}</p>
-                <p>Asset Class: {asset.assetClass}</p>
-              </div>
-=======
     <div className="list">
       <input
         type="text"
@@ -70,7 +47,6 @@ const Assets = () => {
               <p>{asset.description}</p>
               <p>Ticker: {asset.ticker}</p>
               <p>Asset Class: {asset.assetClass}</p>
->>>>>>> c2770447c197bb5b3cbd54f779eacdb46b6a8aa9
             </div>
           </Link>
         ))}
