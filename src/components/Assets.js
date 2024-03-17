0f4adb6debe96fb093395922de74d9bcbcea23a6
SHRAVANI KAWARE <Shravanikaware12@gmail.com>
2024-03-17 20:44:05 +0530

    commit c

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -9,8 +9,8 @@ const Assets = () => {
   useEffect(() => {
     const fetchAssets = async () => {
       try {
-        const response = await axios.get('https://limitless-hackathon-backend.onrender.com/asset');
-        setAssets(response.data.assets);
+        const response = await axios.get('https://limitless-hackathon-backend.onrender.com/order/findListings?assetclass=Equity');
+        setAssets(response.data.orders);
       } catch (error) {
         console.error('Error fetching assets:', error);
       }
@@ -26,27 +26,32 @@ const Assets = () => {
     if (!searchTerm) {
       return true;
     }
-    return (asset.name && asset.name.toLowerCase().includes(searchTerm.toLowerCase())) ||
-           (asset.ticker && asset.ticker.toLowerCase().includes(searchTerm.toLowerCase()));
+    return (asset.assetId.name && asset.assetId.name.toLowerCase().includes(searchTerm.toLowerCase())) ||
+           (asset.assetId.ticker && asset.assetId.ticker.toLowerCase().includes(searchTerm.toLowerCase()));
   });
 
   return (
-    <div className="list">
-      <input
-        type="text"
-        placeholder="Search assets..."
-        value={searchTerm}
-        onChange={handleSearchChange}
-      />
-      {filteredAssets.map(asset => (
-        <Link to={`/assets/${asset._id}`} key={asset._id} className="card-link">
-          <div className="card">
-            <img src={asset.logo} alt={asset.name} />
-            <div className="card-content">
-              <h2>{asset.name}</h2>
-              <p>{asset.description}</p>
-              <p>Ticker: {asset.ticker}</p>
-              <p>Asset Class: {asset.assetClass}</p>
+    <div className="container flex flex-col  mx-auto p-4">
+      <div className="flex flex-col items-center mb-4">
+        <input
+          type="text"
+          placeholder="Search assets..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className="w-64 p-2 border border-gray-300 rounded mr-4"
+        />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {filteredAssets.map(asset => (
+          <Link to={`/assets/${asset._id}`} key={asset._id} className="block">
+            <div className="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+              <img src={asset.assetId.logo} alt={asset.assetId.name} className="w-full h-48 object-cover rounded-t-lg" />
+              <div className="p-4">
+                <h2 className="text-lg font-semibold">{asset.assetId.name}</h2>
+                <p className="text-gray-600">{asset.description}</p>
+                <p>Ticker: {asset.assetId.ticker}</p>
+                <p>Asset Class: {asset.assetClass}</p>
+              </div>
             </div>
           </Link>
         ))}
