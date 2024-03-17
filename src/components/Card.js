551dcb0ec334e5fba88e34cd715bc2694330a551
SHRAVANI KAWARE <Shravanikaware12@gmail.com>
2024-03-17 11:50:46 +0530

    marketpalce

---
+++ src/components/Card.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../App.css'
+import Navbar from './Navbar';
+
+const AssetCard = ({ asset }) => {
+  return (
+    <div className="bg-white rounded-lg shadow-md p-6 mb-6">
+      <h3 className="text-xl font-semibold mb-2">
+        {asset.assetId.name} ({asset.assetId.ticker})
+      </h3>
+      <p className="text-gray-600 mb-2">{asset.assetId.description}</p>
+      <p className="text-gray-600 mb-1">
+        <span className="font-semibold">Asset Class:</span> {asset.assetClass}
+      </p>
+      <p className="text-gray-600 mb-1">
+        <span className="font-semibold">Quantity:</span> {asset.quantity}
+      </p>
+      <p className="text-gray-600 mb-1">
+        <span className="font-semibold">Price:</span> {asset.price}
+      </p>
+      <p className="text-gray-600 mb-1">
+        <span className="font-semibold">Status:</span> {asset.status}
+      </p>
+      <p className="text-gray-600">
+        <span className="font-semibold">Date:</span>{' '}
+        {new Date(asset.date).toLocaleString()}
+      </p>
+    </div>
+  );
+};
+
+const Card = ({ assets }) => {
+  return (
+      <>
+     
+      <Navbar/>
+    <div className="container mx-auto px-4">
+      <h1 className="text-3xl font-bold mb-6"> List</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {assets.map((asset) => (
+          <AssetCard key={asset._id} asset={asset} />
+        ))}
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Card;
--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Card from './Card';
 
 const Marketplace = () => {
-  return (
-    <div>Marketplace</div>
-  )
-}
+  const [assets, setAssets] = useState([]);
 
-export default Marketplace
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://limitless-hackathon-backend.onrender.com/order/findListings?assetclass=Equity');
+        const data = await response.json();
+        setAssets(data.orders);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Card assets={assets} />
+    </div>
+  );
+};
+
+export default Marketplace;
